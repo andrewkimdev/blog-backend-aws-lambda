a314c99ca472937fb31c7bb4c946a5c83c0970c1
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-26 15:10:07 +0900

    [Feature] Add jwt token signer

---
+++ src/functions/auth/handlers/helpers/jwt-signer.ts
@@ -0,0 +1,20 @@
+import * as jwt from 'jsonwebtoken';
+import { db } from '@libs/mysqldb.connection';
+
+export const getUserToken = async (user: { email: string; id: number; roles: string[]}) => {
+  const _userRoleQuery: string = 'SELECT r.name FROM user_roles ur JOIN roles r ON ur.role_id = r.id WHERE ur.user_id = ?'
+  const _userRoleQueryRes = await db.query(_userRoleQuery, [user.id]);
+  user.roles = _userRoleQueryRes.map(({ name }) => name);
+
+  const { JWT_PRIVATE_SECRET, JWT_EXPIRES_IN, JWT_ISSUER } = process.env;
+
+  const payload = {
+    email: user.email,
+    roles: user.roles
+  };
+
+  return jwt.sign(payload, JWT_PRIVATE_SECRET, {
+    expiresIn: JWT_EXPIRES_IN,
+    issuer: JWT_ISSUER,
+  });
+}
--- src/functions/auth/handlers/login-handler.ts
+++ src/functions/auth/handlers/login-handler.ts
@@ -1,31 +1,27 @@
-import { userLoginCredentialsError } from '@functions/auth/handlers/helpers/login-credential-validator';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
-import * as jwt from 'jsonwebtoken'
-import 'dotenv';
+
+import { getUserToken } from './helpers/jwt-signer';
+import { validateUserLoginCredentials } from './helpers/login-credential-validator';
+
+import { HttpStatus } from '@libs/status-code.type';
+
 
 export const login: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
   const body: { email: string; password: string } = JSON.parse(event.body as string);
   const { email, password } = body;
 
-
   // 1. Verify user login credential.
-  const userCredentialCheckError = await userLoginCredentialsError(email, password);
-  if (userCredentialCheckError) {
-    return userCredentialCheckError;
+  const { user, error: loginError } = await validateUserLoginCredentials(email, password);
+  if (loginError) {
+    return loginError;
   }
 
-  // 2. Login Success. Give JWT Token to user. Redirection must happen in user end.
-  const privateKey = process.env.JWT_PRIVATE_KEY;
-  const payload = {
-    foo: 'bar'
-  };
-  const token = jwt.sign(payload, privateKey)
-  console.log(token);
+  // 2. Get signed jwt to user
+  const token: string = await getUserToken(user);
 
   return formatJSONResponse({
     token,
-    body: event.body,
-    event,
-  });
+    message: 'Login success!'
+  }, HttpStatus.OK);
 };
