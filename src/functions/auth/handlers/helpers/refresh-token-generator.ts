74a477c648a72d1d0add6a103c31b48fc52142a0
Andrew KIM <andrewbkim.dev@gmail.com>
2023-11-26 20:02:26 +0900

    [Refactor] Merge multiple queries into a single upsert query

--- src/functions/auth/handlers/helpers/refresh-token-generator.ts
+++ src/functions/auth/handlers/helpers/refresh-token-generator.ts
@@ -10,30 +10,16 @@ export const generateAndStoreRefreshTokenForUserId = async (userId: number): Pro
   const refreshToken = generateRandomString(127);
   const expiresInSeconds = 60 * 60 * 24 * (+process.env.REFRESH_TOKEN_LIFETIME_IN_DAYS || 1);
   const expiresIn = now() + expiresInSeconds;
+  const upsertRefreshTokenQuery: string = `INSERT INTO refresh_tokens (user_id, token, expiresIn, issuedAt, invalidated) 
+                                                VALUES (?, ?, ?, ?, ?)
+                                                ON DUPLICATE KEY UPDATE 
+                                                token = VALUES(token), 
+                                                expiresIn = VALUES(expiresIn), 
+                                                issuedAt = VALUES(issuedAt), 
+                                                invalidated = VALUES(invalidated)`
+  const { affectedRows } = await db.query(upsertRefreshTokenQuery, [userId, refreshToken, expiresIn, now(), false]);
 
-  const saved = await db.transaction(async (_db) => {
-    // 1. See if record already exists.
-    const existsRefreshTokenQuery = 'SELECT COUNT(*) FROM refresh_tokens WHERE user_id = ?';
-    return _db.getrow<any>(existsRefreshTokenQuery, [userId]).then((existsRefreshTokenQueryRes) => {
-      const existsRefreshTokenInDb = +existsRefreshTokenQueryRes?.['count(*)'];
-
-      if (existsRefreshTokenInDb === 0) {
-        // 2. If not exists, simply insert.
-        const insertRefreshTokenQuery: string = 'INSERT INTO refresh_tokens (user_id, token, expiresIn, issuedAt, invalidated) values(?, ?, ?, ?, ?)';
-        return _db.insert(insertRefreshTokenQuery, [userId, refreshToken, expiresIn, now(), false]);
-      } else {
-        // 3. If exists, update row.
-        const updateRefreshTokenQuery: string = 'UPDATE refresh_tokens SET token = ?, expiresIn = ?, issuedAt = ?, invalidated = ? WHERE user_id = ?';
-        return _db.update(updateRefreshTokenQuery, [refreshToken, expiresIn, now(), false, userId]);
-      }
-    }).then((affectedRows) => {
-      return affectedRows > 0;
-    }).catch((err) => {
-      console.error(err);
-    });
-  });
-
-  return saved
+  return affectedRows > 0
     ? {
       refreshToken,
       expiresIn,
