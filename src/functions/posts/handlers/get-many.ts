3ad9709c83db1b55c7f4dd769f02320e536fedd2
Byung-In KIM <kimbi@cliensoft.com>
2023-11-29 09:54:30 +0900

    [Lint] Remove unused code snippets along mongodb access

--- src/functions/posts/handlers/get-many.ts
+++ src/functions/posts/handlers/get-many.ts
@@ -1,13 +1,10 @@
 import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { mongodbHandler } from '@libs/mongodb.connection';
 
 export const getMany: ValidatedEventAPIGatewayProxyEvent<void> = async (event) => {
-  const res = await mongodbHandler();
-
   return formatJSONResponse({
     message: `[GET] POSTS MANY MANY`,
     method: event.httpMethod,
     event,
-    res,
+    // res,
   });
 };
